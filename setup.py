6e053a0bda48f74d4897f30233d3658d0f7a9a05
kidig <kiddima@gmail.com>
2016-06-09 16:29:07 -0700

    fix distribution

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='phuntic',
     description='Python Object Serializer (JSON wrapper)',
-    version='0.1.0',
+    version='0.1.1',
     author='Dmitrii Gerasimenko',
     author_email='kiddima@gmail.com',
     url='https://github.com/kidig/phuntic',
-    packages=find_packages(exclude=['phuntic']),
+    packages=['phuntic'],
 )
